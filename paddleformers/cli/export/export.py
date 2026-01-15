9bd054772ef4ccc61b552742a38cb17c47590e6f
BossPi <30430378+BossPi@users.noreply.github.com>
2026-01-15 14:01:00 +0800

    [bug fix]修复笔误 (#3562)

--- paddleformers/cli/export/export.py
+++ paddleformers/cli/export/export.py
@@ -40,6 +40,10 @@ def check_download_repo(model_name_or_path, download_hub=None):
             if "torch_dtype" in config_dict:
                 print("Loading local model which contains torch dtype.")
     else:
+        # check repo id
+        if download_hub is None:
+            download_hub = os.environ.get("DOWNLOAD_SOURCE", "huggingface")
+            logger.info(f"Using download source: {download_hub}")
         model_name_or_path = check_repo(model_name_or_path, download_hub)
 
     return model_name_or_path
