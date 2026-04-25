e92967e3195101a9066ccdf2b24b3cb3e15e9c51
Wennie396 <wn295448@buaa.edu.cn>
2026-04-25 11:35:42 +0800

    update fleet version (#4342)

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ try:
         install_requires=REQUIRED_PACKAGES,
         entry_points={"console_scripts": get_console_scripts()},
         extras_require={
-            "paddlefleet": ["paddlefleet==0.3.0.dev20260419"],
+            "paddlefleet": ["paddlefleet==0.3.0.dev20260425"],
         },
         python_requires=">=3.8",
         classifiers=[
